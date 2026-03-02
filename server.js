24a7de8d548c761628411030fff16331882bf8fe
Ramatechcodes <ramadanayinla93@gmail.com>
2026-03-02 08:57:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -51,9 +51,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
-  connectionTimeout: 10000, // 10s safety
-  greetingTimeout: 10000,
-  socketTimeout: 10000
+ connectionTimeout: 20000,
+greetingTimeout: 20000,
+socketTimeout: 20000
 });
 
 // Verify on startup
