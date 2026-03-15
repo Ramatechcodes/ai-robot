926ec923ed6836ebe8f78d0b2d906a98a38baed8
Ramatechcodes <ramadanayinla93@gmail.com>
2026-03-16 00:42:31 +0100

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -159,14 +159,8 @@ function safeRender(text) {
   text = text.replace(/__([^_]+)__/g, "$1");    // bold alternative
   text = text.replace(/_(.*?)_/g, "$1");        // italic alternative
 
- // Convert markdown code blocks properly
-text = text.replace(/```(\w+)?([\s\S]*?)```/g, function(match, lang, code){
-
-lang = lang || "javascript";
-
-return `<pre><code class="language-${lang}">${code.replace(/</g,"&lt;").replace(/>/g,"&gt;")}</code></pre>`;
-
-});
+ // code blocks
+  text = text.replace(/```([\s\S]*?)```/g, "<pre><code>$1</code></pre>");
   // Replace newlines with <br>
   return text.replace(/\n/g, "<br>");
 }
